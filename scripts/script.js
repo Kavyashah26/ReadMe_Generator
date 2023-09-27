96fd0c86fe83cedcd3008dbb7ba60a5ebe25ba9e
Kavya Shah <kavyaatulshahpatan@gmail.com>
2023-09-27 23:29:37 +0530

    Created Home page

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("username-form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        // Get the entered GitHub username
+        const username = document.getElementById("github-username").value;
+
+        // Redirect to the next page with the username as a query parameter
+        window.location.href = `next-page.html?username=${username}`;
+    });
+});

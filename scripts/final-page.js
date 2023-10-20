61ca6c1219aa99a66d84c85d82fa53c1e7ab0fb0
Kavya Shah <kavyaatulshahpatan@gmail.com>
2023-10-20 12:43:23 +0530

    Created final page and download button

--- scripts/final-page.js
+++ scripts/final-page.js
@@ -1,17 +1,14 @@
- // Function to get the value of a query parameter from the URL
- //  function getQueryParam(name) {
- // const urlParams = new URLSearchParams(window.location.search);
-//     console.log(urlParams.get(name));
-//     return urlParams.get(name);
-// }
-
+// Function to parse URL parameters
 function parseURLParams(url) {
-    // console.log("in"); 
     var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        queryEnd = url.indexOf("#") + 1 || url.length + 1,
         query = url.slice(queryStart, queryEnd - 1),
         pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
+        parms = {},
+        i,
+        n,
+        v,
+        nv;
 
     if (query === url || query === "") return;
 
@@ -23,73 +20,136 @@ function parseURLParams(url) {
         if (!parms.hasOwnProperty(n)) parms[n] = [];
         parms[n].push(nv.length === 2 ? v : null);
     }
-    // console.log(typeof parms);
+
     return parms;
 }
-// parseURLParams("www.mints.com?name=something")
 
+// Get URL parameters
 const urlParams = new URLSearchParams(window.location.search);
-const url="?"+urlParams.toString();
+const url = "?" + urlParams.toString();
 const data = parseURLParams(url);
 
-// const currentlyWorkingOn = getQueryParam('currentlyWorkingOn');
-// const lookingToCollaborate = getQueryParam('lookingToCollaborate');
-// const lookingForHelp = getQueryParam('lookingForHelp');
-// const currentlyLearning = getQueryParam('currentlyLearning');
-// const askMeAbout = getQueryParam('askMeAbout');
-// const funFact = getQueryParam('funFact');
+// Define GitHub API URL
+const username = data.username || "Kavyashah26"; // Default to Kavyashah26 if username is not provided
+const api_url = `https://api.github.com/users/${username}`;
 
-console.log(data);
-// console.log(url.indexOf("?")+1);
-
-// console.log(data);
-
-for (const i in data) {
-    // console.log(i);
-    print(data[i])
-}
-
-// const gitapi=
-const api_url = 
-      "https://api.github.com/users/Kavyashah26";
- 
-// Defining async function
-async function getapi(url) {
-   
-    // Storing response
+// Function to get GitHub data using the GitHub API
+async function getGitHubData(url) {
     const response = await fetch(url);
-   
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    // if (response) {
-    //     // hideloader();
-    // }
-    // show(data);
-    // console.log(typeof data);
-    return data
+    const data = await response.json();
+    return data;
 }
-// Calling that async function
-const apidata=getapi(api_url);
 
-// console.log(apidata);
+// Function to generate README content
+async function generateReadme() {
+    // Get GitHub data
+    const githubData = await getGitHubData(api_url);
 
-function show(apidata) {
-    for (const i in apidata) {
-        console.log(i);
-        print(apidata[i])
-    }
+    // Parse URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const url = "?" + urlParams.toString();
+    const customText = parseURLParams(url);
+    // const additionalData = parseURLParams(window.location.href);
+
+    // Extract relevant data from URL parameters
+    // const customText = additionalData.customText || "No custom text provided";
+
+    // const urlParams = new URLSearchParams(window.location.search);
+
+    // Snake animation
+    const snakeAnimation = '<img src="https://raw.githubusercontent.com/maurodesouza/maurodesouza/output/snake.svg" alt="Snake animation" />';
+
+    // Languages graph
+    const languagesGraph = `<img src="https://github-readme-stats.vercel.app/api/top-langs?username=${githubData.login}&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=light&hide_border=false" height="150" alt="languages graph" />`;
+
+    // Selected Skills
+    // const selectedSkills = [
+    //     { name: "JavaScript", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+    //     { name: "TypeScript", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
+    //     { name: "React", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+    //     // Add more skills as needed
+    // ];
+
+    const skillsSection = selectedSkills.length > 0
+        ? `\n\n${selectedSkills.map(skill => ` <img src="${skill.image}" height="30" alt="${skill.name}" class="skillImage"> `).join("")}`
+        : '';
+
+    // Generate README content
+    const readmeContent = `# ${githubData.name}'s GitHub Profile
+
+${snakeAnimation}
+
+![GitHub Stats](https://github-readme-stats.vercel.app/api?username=${githubData.login}&show_icons=true)
+
+## About Me
+
+- **Bio:** ${githubData.bio || "N/A"}
+- **Location:** ${githubData.location || "N/A"}
+
+## Skills
+
+${skillsSection}
+
+## Additional Information
+
+- **Custom Text:** ${customText}
+
+## Contact Me
+
+- **Email:** ${githubData.email || "N/A"}
+
+`;
+
+    return readmeContent;
 }
-// print(data[0])
 
-function print(name) {
-    if (name!="") {   
-        // console.log(currentlyLearning);
-        // Display the username on the page
-        console.log("1");
-        const data = document.getElementById('test');
-        data.innerHTML+='<br>'
-        data.innerHTML += ` ${name} `;
-    }
-    
+
+// Function to download README file
+function downloadReadme() {
+    generateReadme().then((content) => {
+        const blob = new Blob([content], { type: "text/plain" });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "README.md";
+        link.click();
+    });
 }
+
+// Event listener for the download button
+// const downloadButton = document.getElementById("downloadButton");
+// downloadButton.addEventListener("click", downloadReadme);
+
+
+
+
+// Array to store selected skills
+const selectedSkills = [];
+
+// Function to update selected skills
+function updateSelectedSkills() {
+    selectedSkills.length = 0; // Clear the array
+
+    // Get all checkboxes with a class 'skillCheckbox'
+    const skillCheckboxes = document.querySelectorAll('.skillCheckbox');
+
+    // Iterate through each checkbox and add selected skills to the array
+    skillCheckboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            const skillName = checkbox.getAttribute('data-skill-name');
+            const skillImage = checkbox.getAttribute('data-skill-image');
+            
+            selectedSkills.push({
+                name: skillName,
+                image: skillImage
+            });
+        }
+    });
+}
+
+
+        // Event listener for the "Download README" button
+        const downloadButton = document.getElementById("downloadButton");
+        downloadButton.addEventListener("click", () => {
+            updateSelectedSkills();
+            downloadReadme();
+        });
